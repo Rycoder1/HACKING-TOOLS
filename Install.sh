c55fe0d1a913a4c86fc379e20d575bb1f3606ec4
Rycoder <120586250+Rycoder1@users.noreply.github.com>
2023-01-06 07:54:56 +0530

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+#installation
+
+clear
+
+echo ""
+
+echo ""
+
+echo " SUBSCRIBE MY CHANNEL 🔔 " | lolcat
+
+xdg-open https://youtube.com/@Rycoder_1
+
+echo ""
+
+echo -e "\e[101m Press Enter \e[0m"
+
+read a1
+
+sleep 1
+
+clear
+
+echo "Loading..." | lolcat
+
+sleep 1
+
+clear
+
+echo "Loading..." | lolcat
+
+sleep 1
+
+clear
+
+echo "Loading..." | lolcat
+
+sleep 1
+
+clear
+
+toilet -f future "RY CODER" | lolcat
+
+echo "-------------------------------------------------------" | lolcat
+
+echo   "Created  : Rycoder $white" |lolcat
+
+echo   "Contact  : Instagram.com/rycoder_1 $white" |lolcat
+
+echo   "YouTube  : Rycoder_1 $white" |lolcat
+
+echo "-------------------------------------------------------" | lolcat
+
+trap ctrl_c INT
+
+ctrl_c() {
+
+clear
+
+echo  "Detected, Trying To Exit  ... "
+
+echo ""
+
+sleep 1
+
+echo ""
+
+echo "Rycoder" | lolcat
+
+sleep 1
+
+exit
+
+}
+
+apt update
+
+apt upgrade -y
+
+pkg install ruby -y 
+
+gem install toilet -y 
+
+pkg install lolcat
+
+pkg install figlet
+
+apt install git php openssh curl -y
+
+pkg install python2 -y 
+
+pkg install nano php -y
+
+pip2 install mechanize
+
+pip2 install --upgrade pip
+
+echo ""
+
+echo "CODE_BY_RYCODER" | lolcat
+
+echo "________________________" | lolcat
+
+sleep 10
+
+echo " create account on cashkaro.com "
+
+echo " set your browser "
+
+termux-open-url https://cashkaro.com?r=8026255&fname=Rixon
+
+echo ""
